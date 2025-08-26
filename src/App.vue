5b8548fe016c705b6124d616f7adc5c36f64e6a7
atara-327 <atara327a@gmail.com>
2025-08-26 12:18:17 +0900

    bootstrap使ってみた

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <h1>atara's PROFILE</h1>
-  <p>X: <a href="https://x.com/atara_0327">@atara_0327</a></p>
+  <p><i class="bi bi-twitter-x"></i>: <a href="https://x.com/atara_0327">@atara_0327</a></p>
 </template>
 
 <style scoped></style>
