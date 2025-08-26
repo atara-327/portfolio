50bbf3e8c67c424bc4e2ef6a80c266d7bbdf1275
atara-327 <atara327a@gmail.com>
2025-08-26 14:40:54 +0900

    仮置きー

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>atara's PROFILE</h1>
-  <p><i class="bi bi-twitter-x"></i>: <a href="https://x.com/atara_0327">@atara_0327</a></p>
+  <div class="main container">
+    <h1 class="m-3 text-center">atara's PORTFOLIO</h1>
+    <div class="container text-center main-content">
+      <ul>
+        <li><i class="bi bi-twitter-x"></i> <a href="https://x.com/atara_0327" target="_blank">@atara_0327</a></li>
+        <li><i class="bi bi-github"></i> <a href="https://github.com/atara-327" target="_blank">atara-327</a></li>
+      </ul>
+      <br />
+      <ul>
+        <li><a href="https://sia-song.ataraman.com" target="_blank">依澄しあ 歌枠検索</a></li>
+      </ul>
+    </div>
+</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.main-content {
+  background-color: rgb(35, 17, 41);
+  border-radius: 10px;
+  border: 1px dashed rgb(220, 219, 224);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  padding: 3rem 0;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap';
+import './assets/styles/main.css'
 
 const app = createApp(App)
 
