b551b4aab136840e43f31821464300cd32637cc9
atara-327 <atara327a@gmail.com>
2025-08-26 14:57:22 +0900

    調整

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,22 @@
 
 <template>
   <div class="main container">
-    <h1 class="m-3 text-center">atara's PORTFOLIO</h1>
-    <div class="container text-center main-content">
+    <h1 class="my-5 text-center">atara's PORTFOLIO</h1>
+    <div class="container py-5 text-center border border-white rounded shadow main-content">
       <ul>
         <li><i class="bi bi-twitter-x"></i> <a href="https://x.com/atara_0327" target="_blank">@atara_0327</a></li>
         <li><i class="bi bi-github"></i> <a href="https://github.com/atara-327" target="_blank">atara-327</a></li>
       </ul>
-      <br />
-      <ul>
-        <li><a href="https://sia-song.ataraman.com" target="_blank">依澄しあ 歌枠検索</a></li>
-      </ul>
     </div>
+    <br />
+    <ul>
+      <li><a href="https://sia-song.ataraman.com" target="_blank">依澄しあ 歌枠検索</a></li>
+    </ul>
 </div>
 </template>
 
 <style scoped>
 .main-content {
   background-color: rgb(35, 17, 41);
-  border-radius: 10px;
-  border: 1px dashed rgb(220, 219, 224);
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  padding: 3rem 0;
 }
 </style>
